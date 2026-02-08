2b69d3a95fb522ab297838afec769edce810ca10
Artemiy <artemsikor18@gmail.com>
2026-02-09 01:43:55 +0300

    fix weight

--- skill-map/src/components/Roadmap/Roadmap.tsx
+++ skill-map/src/components/Roadmap/Roadmap.tsx
@@ -28,83 +28,85 @@ function ActiveRoadmap({activeRoadmap, activeKey, activeModule, onCompliteModule
             <Card.Title style={{margin: "20px", alignItems: "center", justifyContent: "center"}}>
                 <h1>Твой текущий роадмап</h1>
             </Card.Title>
-            <Accordion activeKey={activeKey} flush={true} className="mb-3 w-50 mx-auto">
-                {activeRoadmap.path.map(
-                    (step, i) => {
-                        let index = i;
-                        const isActive = activeRoadmap.complited === index
-                        return ( 
+            <Accordion activeKey={activeKey} flush={true} className="mb-3">
+            {activeRoadmap.path.map((step, i) => {
+                let index = i;
+                const isActive = activeRoadmap.complited === index
+                return ( 
+                <>
+                    <div key={step.id} ref={isActive ? activeRef : null}>
+                    <Accordion.Item eventKey={index.toString()}>
+                        <Accordion.Header>
+                        <span style={activeRoadmap.complited !== index ? { color: "#aaa", pointerEvents: "none" } : {}}>
+                            {index < activeRoadmap.complited && (
+                            <span className="ms-2"> ✅</span>
+                            )}{step.name}
+                        </span>
+                        </Accordion.Header>
+                        <Accordion.Body className="p-4">
+                        {step?.skills && step.skills.length > 0 && (
                             <>
-                            <div key={step.id} ref={isActive ? activeRef : null}>
-                                <Accordion.Item eventKey={index.toString()} >
-                                    <Accordion.Header>
-                                        
-                                        <span style={activeRoadmap.complited !== index ? { color: "#aaa", pointerEvents: "none" } : {}}>
-                                            {index < activeRoadmap.complited && (
-                                                <span className="ms-2"> ✅</span>
-                                            )}{step.name}
-                                        </span>
-                                    </Accordion.Header>
-                                    <Accordion.Body>
-                                        {
-                                            step?.skills 
-                                            && step.skills.length > 0 
-                                            && (
-                                                <>
-                                                    {isActive && activeModule && (
-                                                        <>
-                                                            <h3>{activeModule.level}</h3>
-                                                            <p>{activeModule.description}</p>
-                                                            <h5>Полезные ссылки</h5>
-                                                             <ListGroup as="ol" numbered>
-                                                                {activeModule.links.map((link, i) => (
-                                                                    <ListGroup.Item variant="dark"
-                                                                        className="d-flex justify-content-between align-items-start"
-                                                                        key={i}
-                                                                    >
-                                                                        <div className="ms-2 me-auto">
-                                                                            <div className="fw-bold"> <a href={link.url}>{link.title}</a> </div>
-                                                                        </div>
-                                                                    </ListGroup.Item>
-                                                                ))}
-                                                            </ListGroup>
-                                                        </>
-                                                    )}
-                                                    <div className="fw-bold"><h5>Основные идеи модуля</h5> </div>
-                                                    <ListGroup>
-                                                    {step.skills.map(skill => (
-                                                        <ListGroup.Item
-                                                            variant="dark"
-                                                            className="d-flex justify-content-between align-items-start"
-                                                            key={skill.id}
-                                                        >
-                                                            <div className="ms-2 me-auto">
-                                                                {skill.name}
-                                                            </div>
-                                                        </ListGroup.Item>
-                                                    ))}
-                                                    </ListGroup>
-                                                    <br/>
-                                                   
-                                                </>
-                                            )
-                                        }
-                                        {
-                                            isActive && (
-                                                <Button variant="success" id="button-addon2" style={{margin: "10px"}} onClick={(e)=>{onCompliteModule()}}>
-                                                    Завершить модуль!
-                                                </Button>
-                                            )
-                                        }
-                                    </Accordion.Body>
-                                </Accordion.Item>
-                                </div>
+                            {isActive && activeModule && (
+                                <>
+                                <h3 className="mb-3">{activeModule.level}</h3>
+                                <p className="mb-4">{activeModule.description}</p>
+                                <h5 className="mb-3">Полезные ссылки</h5>
+                                <ListGroup as="ol" numbered className="mb-4">
+                                    {activeModule.links.map((link, i) => (
+                                    <ListGroup.Item 
+                                        variant="dark"
+                                        className="d-flex justify-content-between align-items-start p-3"
+                                        key={i}
+                                    >
+                                        <div className="ms-2 me-auto">
+                                        <div className="fw-bold">
+                                            <a href={link.url} className="text-decoration-none">
+                                            {link.title}
+                                            </a>
+                                        </div>
+                                        </div>
+                                    </ListGroup.Item>
+                                    ))}
+                                </ListGroup>
+                                </>
+                            )}
+                            <div className="fw-bold mb-3">
+                                <h5>Основные идеи модуля</h5>
+                            </div>
+                            <ListGroup className="mb-4">
+                                {step.skills.map(skill => (
+                                <ListGroup.Item
+                                    variant="dark"
+                                    className="d-flex justify-content-between align-items-start p-3"
+                                    key={skill.id}
+                                >
+                                    <div className="ms-2 me-auto">
+                                    {skill.name}
+                                    </div>
+                                </ListGroup.Item>
+                                ))}
+                            </ListGroup>
                             </>
-                        )
-                    }
-                )}
-                
+                        )}
+                        {isActive && (
+                            <Button 
+                            variant="success" 
+                            id="button-addon2" 
+                            style={{margin: "10px"}} 
+                            onClick={onCompliteModule}
+                            className="w-100 w-md-50"
+                            >
+                            Завершить модуль!
+                            </Button>
+                        )}
+                        </Accordion.Body>
+                    </Accordion.Item>
+                    </div>
+                </>
+                )
+            })}
             </Accordion>
+
         </>
     )
 }
